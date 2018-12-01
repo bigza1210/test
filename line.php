07d4aabbbf08cd75dc5cb2c1d6423a0ac1c5fafa
bigza1210 <bigza1210@hotmail.co.th>
2018-12-01 08:53:19 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '5NrEfSd+/UuQ8893em1owmgksRosRZrL/c85phujo8+e6HUFbuJcUKyMsIdSLNFDj0ie2X8w9INktGVx6gACCXqWhzGKkFFralsnHVdO+ko0iXkcOCedgPNa8MzFMjNnDhPX0/UmvztO16oywis1KwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Ubb5381aa171f4e977f61e2dee1bbf422',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
